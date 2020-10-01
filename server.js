f1afb63496532a15568e5fb6bedb7f68c22299c7
Narasimha Kamath <narasimhakamath@outlook.com>
2020-10-02 01:47:17 +0530

    Configured the express server, and made changes to the environment variables.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+
+dotenv.config({path: './configurations/configuration.env'});
+
+const app = express();
+app.use(express.json());
+
+if(process.env.ENVIRONMENT === "development")
+	app.use(morgan('dev'));
+
+const PORT = process.env.PORT || 5000;
+console.clear();
+const server = app.listen(PORT, console.log(`Server is running in the environment: ${process.env.ENVIRONMENT} on the port: ${PORT}`.yellow.bold));
+
+process.on('unhandledRejection', (error, promise) => {
+	console.log(`Error: ${error.message}`.red.bold);
+	server.close(() => process.exit(1));
+});
